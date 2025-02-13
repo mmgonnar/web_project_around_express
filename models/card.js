2361824516a7cd7dfa6ec61a801d79fb07e03731
mmgonnar <gn.mariela@gmail.com>
2025-02-13 17:20:01 -0600

    Finnished cardSchema

--- models/card.js
+++ models/card.js
@@ -23,10 +23,12 @@ const cardSchema = new mongoose.Schema({
     ref: "owner",
     required: true,
   },
+  likes: {
+    type: [Schema.Types.ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
-
-// name: nombre de la tarjeta, string de 2 a 30 caracteres, campo obligatorio.
-// link: enlace a la imagen, string, campo obligatorio. Utiliza la expresión regular del esquema user para validar los datos de entrada.
-// owner: enlace al modelo del autor de la tarjeta, tipo ObjectId, campo obligatorio.
-// likes: una lista de usuarios que dieron like a la publicación, un array ObjectId, un array vacío por defecto (default field)
-// createdAt: fecha de creación, tipo Date, valor por defecto Date.now
