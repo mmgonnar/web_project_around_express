d7288f2a807bf4c081a8d48820aac7aa588f1afc
mmgonnar <gn.mariela@gmail.com>
2025-02-13 08:07:20 -0600

    Added some stuffs

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const userRoutes = require("./routes/users");
 const cardRoutes = require("./routes/cards");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// 4 cors
+const settings = {
+  origin: "https:localhost/3000",
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type",
+};
 
+//Cors Middleware
+app.use(cors(settings));
+//Middleware to parse JSON
 app.use(express.json());
+//Middleware to get info
+app.use((req, res) => {
+  console.log(`${new Date().toLocaleString()}, ${req.method}, ${req.url}`);
+});
 
 //root
 app.get("/", (req, res) => {
