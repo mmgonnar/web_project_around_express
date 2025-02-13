6188322fb78b7948fe6ab9e15f23222567966c2f
mmgonnar <gn.mariela@gmail.com>
2025-02-13 12:36:44 -0600

    Added new user routes

--- routes/users.js
+++ routes/users.js
@@ -3,55 +3,36 @@ const router = express.Router();
 const fsPromises = require("node:fs/promises");
 const fs = require("fs");
 const path = require("path");
-
-router.get("/", (req, res) => {
-  fsPromises
-    .readFile(path.join(__dirname, "../data/users.json"))
-    .then((data) => {
-      const users = JSON.parse(data);
-      res.send(users);
-    });
-});
+const { getUsers, getUserById, createUser } = require("../controllers/user");
 
 // router.get("/", (req, res) => {
-//   res.json(users);
+//   fsPromises
+//     .readFile(path.join(__dirname, "../data/users.json"))
+//     .then((data) => {
+//       const users = JSON.parse(data);
+//       res.send(users);
+//     });
 // });
+router.get("/user", getUsers);
+router.get("/user", getUserById);
+router.post("/users", createUser);
 
-router.get("/:id", (req, res) => {
-  const users = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "..", "data", "users.json"))
-  );
+// router.get("/:id", (req, res) => {
+//   const users = JSON.parse(
+//     fs.readFileSync(path.join(__dirname, "..", "data", "users.json"))
+//   );
 
-  const { id } = req.params;
-  //console.log("user w/id");
+//   const { id } = req.params;
 
-  //const users = JSON.parse(data);
-  const user = users.find((user) => user._id === id);
+//   const user = users.find((user) => user._id === id);
 
-  if (!user) {
-    return res.status(404).send({
-      message: "User not found",
-    });
-  }
-  res.send({
-    user,
-  });
-});
-
-// router.post("/", (req, res) => {
-//   const { name, age } = req.body;
-
-//   if (!name || !age) {
-//     massage: "Name & age required";
+//   if (!user) {
+//     return res.status(404).send({
+//       message: "User not found",
+//     });
 //   }
-
-//   const newUser = {
-//     id: user.lenght + 1,
-//     name,
-//     age,
-//   };
-//   res.status(201).send({
-//     message: "User created",
+//   res.send({
+//     user,
 //   });
 // });
 
